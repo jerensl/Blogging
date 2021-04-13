5294168dcee0df9b0b94142a8bf1c545e5fe4c35
jerensl <jerenslensun@gmail.com>
2021-04-13 11:28:18 +0800

    setup package and dependencies AB#30

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="px-20">
+      <Head>
+        <title>Jerens Lensun</title>
+      </Head>
+
+      <main className="h-screen text-center">
+        <h1 className="p-40 font-bold text-3xl">
+          This Page is Under Development
+        </h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
