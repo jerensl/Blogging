6307f7ee8fbfabd1fdf06170610cf6cf840e4ed1
Jerens Lensun <jerensslensun@gmail.com>
2021-07-06 10:19:48 +0800

    adding sitemap for seo AB#44 fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="IcqLpRy2zwKkTN1TlYABjrsUKoOOUp5sE5EQoMhrg_U"
+          />
+        </Head>
+
         <meta name="application-name" content={APP_NAME} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -31,10 +37,7 @@ class MyDocument extends Document {
         <meta name="theme-color" content="#FFFFFF" />
         {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
         {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
-        <meta
-          name="google-site-verification"
-          content="IcqLpRy2zwKkTN1TlYABjrsUKoOOUp5sE5EQoMhrg_U"
-        />
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
